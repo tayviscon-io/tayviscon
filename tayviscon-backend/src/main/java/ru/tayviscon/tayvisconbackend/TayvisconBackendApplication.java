60a0e01e0414318eb508a5cbe5dd7fa259bd66be
Tyomych Tovkach <tyomych.tovkach@tayviscon.ru>
2023-03-17 08:54:39 +0300

    Создал клиент для Tayviscon Renderer сервиса посредством Spring HATEOAS
    - Изменил application.properties -> application.yml, где хранятся настройки приложения: прописан url для сервиса Tayviscon Renderer;
    - Добавил SiteProperties.java, для отображения конфигурацицнных настроек приложения, прописанных в application.yml, в объектной модели: url ссылки на Tayviscon Renderer сервис;
    - Изменил TayvisconBackendApplication.java, для добавления функциональности сканирования конфигурационных настроек посредством аннотации @ConfigurationPropertiesScan;
    - Добавил TayvisconRendererClient.java, для взаимодействия с сервисом Tayviscon Renderer посредством отправки запросов, с учетом, что сервис обеспечивает динамический доступ через гипермедиа.
    
    Изменил pom.xml:
    * Добавил зависимость на Spring HATEOAS;
    * Добавил зависимость на Spring Boot Configuration Processor;

--- tayviscon-backend/src/main/java/ru/tayviscon/tayvisconbackend/TayvisconBackendApplication.java
+++ tayviscon-backend/src/main/java/ru/tayviscon/tayvisconbackend/TayvisconBackendApplication.java
@@ -2,12 +2,12 @@ package ru.tayviscon.tayvisconbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TayvisconBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TayvisconBackendApplication.class, args);
     }
-
 }
---
+++ tayviscon-backend/src/main/java/ru/tayviscon/tayvisconbackend/renderer/TayvisconRendererClient.java
@@ -0,0 +1,50 @@
+package ru.tayviscon.tayvisconbackend.renderer;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Hop;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import ru.tayviscon.tayvisconbackend.SiteProperties;
+
+import java.net.URI;
+import java.util.Arrays;
+
+@Component
+public class TayvisconRendererClient {
+
+    private static final ParameterizedTypeReference<CollectionModel<GuideMetadata>> guidesResourceRef =
+            new ParameterizedTypeReference<CollectionModel<GuideMetadata>>() {};
+    private static final MediaType TEXT_ASCIIDOC = MediaType.parseMediaType("text/asciidoc");
+    private final RestTemplate restTemplate;
+
+    private final Traverson traverson;
+
+    public TayvisconRendererClient(RestTemplateBuilder builder, SiteProperties properties) {
+        this.restTemplate = builder
+                .messageConverters(Traverson.getDefaultMessageConverters(MediaTypes.HAL_JSON))
+                .build();
+        this.traverson = new Traverson(URI.create(properties.getRenderer().getServiceUrl()), MediaTypes.HAL_JSON);
+        this.traverson.setRestOperations(restTemplate);
+    }
+
+    public GuideMetadata[] getAllGuides() {
+        return traverson.follow("guides").toObject(guidesResourceRef).getContent().toArray(new GuideMetadata[]{});
+    }
+
+    public GuideMetadata[] getGettingStartedGuides() {
+        return Arrays.stream(getAllGuides())
+                .filter(guide -> GuideType.GETTING_STARTED.equals(guide.getType()))
+                .toArray(GuideMetadata[]::new);
+    }
+
+    public GuideMetadata getGettingStartedGuide(String name) {
+        return this.traverson.follow("guides")
+                .follow(Hop.rel("getting-started").withParameter("guide", name))
+                .toObject(GuideMetadata.class);
+    }
+}
