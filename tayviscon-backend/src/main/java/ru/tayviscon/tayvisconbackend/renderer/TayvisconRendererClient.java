49cf42cb78d4eb32cd372bf45dfe6062c59128aa
Tyomych Tovkach <tyomych.tovkach@tayviscon.ru>
2023-03-19 15:28:59 +0300

    Создал обработку для руководств типа Topical:
        - Добавил TopicalGuidesRepository.java, как реализацию GuidesRepository для хранения руководств типа "Topical";
        - Добавил TopicalGuideController.java, для обслуживания всех запросов, связанных с руководствами типа Tutorial;
        - Добавил TopicalGuide.java,как модель для работы с руководствами типа "Topical";
    
    Изменил TayvisconRendererClient.java:
    
        - Добавил методы, обеспечивающие  работу с руководствами типа Tutorial:  взаимодействие с сервисом Tayviscon Renderer просходит посредством отправки запросов, с учетом, что сервис обеспечивает динамический доступ через гипермедиа.

--- tayviscon-backend/src/main/java/ru/tayviscon/tayvisconbackend/renderer/TayvisconRendererClient.java
+++ tayviscon-backend/src/main/java/ru/tayviscon/tayvisconbackend/renderer/TayvisconRendererClient.java
@@ -73,4 +73,23 @@ public class TayvisconRendererClient {
                 .follow("content")
                 .toObject(GuideContent.class);
     }
+
+    public GuideMetadata[] getTopicalGuides() {
+        return Arrays.stream(getAllGuides())
+                .filter(guide -> GuideType.TOPICAL.equals(guide.getType()))
+                .toArray(GuideMetadata[]::new);
+    }
+
+    public GuideMetadata getTopicalGuide(String name) {
+        return traverson.follow("guides")
+                .follow(Hop.rel("topical").withParameter("guide", name))
+                .toObject(GuideMetadata.class);
+    }
+
+    public GuideContent getTopicalGuideContent(String name) {
+        return traverson.follow("guides")
+                .follow(Hop.rel("topical").withParameter("guide", name))
+                .follow("content")
+                .toObject(GuideContent.class);
+    }
 }
